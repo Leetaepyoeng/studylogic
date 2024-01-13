ffa4954039129c70acdf1249e0a8004a51755d23
Leetaepyoeng <48676338+Leetaepyoeng@users.noreply.github.com>
2024-01-13 20:16:33 +0900

    백준 단계별로풀어보기 정렬

--- src/com/test/tab/study14/Main.java
+++ src/com/test/tab/study14/Main.java
@@ -1,24 +1,43 @@
 package com.test.tab.study14;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Scanner;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
 public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException {
+		//소트인사이드
+		
+		
+		//수 정렬하기 3
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		StringBuilder sb = new StringBuilder();
+//		
+//		int cnt = Integer.parseInt(br.readLine());
+//		int[] num = new int[cnt];
+//		for(int i = 0; i < cnt; i++) 
+//			num[i] = Integer.parseInt(br.readLine());
+//		Arrays.sort(num);
+//		for(int i = 0; i < cnt; i++) 
+//			sb.append(num[i]).append('\n');
+//		System.out.println(sb);
+		
+		
 		//수 정렬하기 2
-		Scanner scan = new Scanner(System.in);
-		StringBuilder sb = new StringBuilder();
-		//StringBuilder:가변(mutable)한 문자열을 처리하기 위한 클래스, 새로운 문자열을 추가하거나 변경하게 되면 “기존의 객체”로 추가 및 변경.
-		int cnt = scan.nextInt();
-		// list 계열 중 하나를 쓰면 된다.
-		ArrayList<Integer> list = new ArrayList<>();//리스트에 대해서 좀 더 알아보기
-		for(int i =0; i < cnt; i++) 
-			list.add(scan.nextInt());
-		Collections.sort(list);//객체의 모음, 그룹
-		for(int value : list) 
-			sb.append(value).append('\n');
-		System.out.println(sb);
+//		Scanner scan = new Scanner(System.in);
+//		StringBuilder sb = new StringBuilder();
+//		//StringBuilder:가변(mutable)한 문자열을 처리하기 위한 클래스, 새로운 문자열을 추가하거나 변경하게 되면 “기존의 객체”로 추가 및 변경.
+//		int cnt = scan.nextInt();
+//		// list 계열 중 하나를 쓰면 된다.
+//		ArrayList<Integer> list = new ArrayList<>();//리스트에 대해서 좀 더 알아보기
+//		for(int i =0; i < cnt; i++) 
+//			list.add(scan.nextInt());
+//		Collections.sort(list);//객체의 모음, 그룹
+//		for(int value : list) 
+//			sb.append(value).append('\n');
+//		System.out.println(sb);
+		
 		
 		
 		//커트라인
