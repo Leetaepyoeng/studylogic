57d80a007b4841c319163b5bd938b27a2a133b7d
Leetaepyoeng <48676338+Leetaepyoeng@users.noreply.github.com>
2024-01-14 14:04:08 +0900

    백준 단계별로풀어보기 정렬

--- src/com/test/tab/study14/Main.java
+++ src/com/test/tab/study14/Main.java
@@ -1,13 +1,24 @@
 package com.test.tab.study14;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException {
+		//좌표 정렬하기
+		
+		
+		
 		//소트인사이드
+//		Scanner scan = new Scanner(System.in);
+//		String str = scan.nextLine();
+//		char[] nums = str.toCharArray();
+//		for(int i = 0; i < str.length(); i++)
+//			nums[i] = str.charAt(i);
+//		Arrays.sort(nums);
+//		for(int i = str.length() - 1; i >= 0; i--)
+//			System.out.print(nums[i]);
 		
 		
 		//수 정렬하기 3
