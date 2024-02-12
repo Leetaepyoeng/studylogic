85fd6aa4cc264989074fe98dac3b82bcba961c43
Leetaepyoeng <48676338+Leetaepyoeng@users.noreply.github.com>
2024-02-13 01:03:49 +0900

    누적합

--- src/com/test/tab/study17/Main.java
+++ src/com/test/tab/study17/Main.java
@@ -7,33 +7,59 @@ import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		//기상청 인턴 신현수
+		//2차원 배열의 합
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		int nHeight = Integer.parseInt(st.nextToken());
+		int nWidth = Integer.parseInt(st.nextToken());
+		int[][] buf = new int[nHeight][nWidth];
+		for(int h = 0; h < nHeight; h++) {
+			st = new StringTokenizer(br.readLine());
+			for(int w = 0; w < nWidth; w++)
+				buf[h][w] = Integer.parseInt(st.nextToken());
+		}
 		
-		int cnt = Integer.parseInt(st.nextToken());
-		int sumCnt = Integer.parseInt(st.nextToken());
-		int[] arrNum = new int[cnt];
-		int maxNum = 0;
-		
-		st = new StringTokenizer(br.readLine());
-		for(int i = 0; i < cnt; i++)
-			arrNum[i] = Integer.parseInt(st.nextToken());
-		
-		for(int i = 0; i < cnt-sumCnt; i++) {
-			int num = 0;
-			for(int j = i; j < i+sumCnt; j++) {
-				num += arrNum[j];
+		int cnt = Integer.parseInt(br.readLine());
+		for(int i = 0; i < cnt; i++) {
+			int sum = 0;
+			st = new StringTokenizer(br.readLine());
+			int sttH = Integer.parseInt(st.nextToken())-1;
+			int sttW = Integer.parseInt(st.nextToken())-1;
+			int endH = Integer.parseInt(st.nextToken());
+			int endW = Integer.parseInt(st.nextToken());
+			for(int h = sttH; h < endH; h++) {
+				for(int w = sttW; w < endW; w++){
+					sum += buf[h][w];
+				}
 			}
-			if(num > maxNum)
-				maxNum = num;
+			sb.append(sum+"\n");
 		}
+		System.out.println(sb);
 		
-		if(cnt == sumCnt) {
-			for(int i = 0; i < cnt; i++)
-				maxNum+=arrNum[i];
-		}
 		
-		System.out.println(maxNum);
+		
+		//기상청 인턴 신현수
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		StringTokenizer st = new StringTokenizer(br.readLine());
+//		
+//		int cnt = Integer.parseInt(st.nextToken());
+//		int sumCnt = Integer.parseInt(st.nextToken());
+//		int[] arrNum = new int[cnt];
+//		
+//		st = new StringTokenizer(br.readLine());
+//		for(int i = 0; i < cnt; i++)
+//			arrNum[i] = Integer.parseInt(st.nextToken());
+//		
+//		int maxNum = -2147483648;
+//		for(int i = 0; i <= cnt-sumCnt; i++) {
+//			int num = 0;
+//			for(int j = i; j < i+sumCnt; j++) {
+//				num += arrNum[j];
+//			}
+//			if(num > maxNum)
+//				maxNum = num;
+//		}
+//		System.out.println(maxNum);
 	}
 }
