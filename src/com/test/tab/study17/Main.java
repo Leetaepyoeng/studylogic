6c6cddc37d6aff327a0f77c9e259b3cbde8946b9
Leetaepyoeng <48676338+Leetaepyoeng@users.noreply.github.com>
2024-02-14 19:45:16 +0900

    누적합

--- src/com/test/tab/study17/Main.java
+++ src/com/test/tab/study17/Main.java
@@ -7,35 +7,65 @@ import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		//2차원 배열의 합
+		//2차원 배열의 합 누적합 사용, 공식을 외우자
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		StringBuilder sb = new StringBuilder();
 		int nHeight = Integer.parseInt(st.nextToken());
 		int nWidth = Integer.parseInt(st.nextToken());
 		int[][] buf = new int[nHeight][nWidth];
+		int[][] pSum = new int[nHeight+1][nWidth+1];
+		
 		for(int h = 0; h < nHeight; h++) {
 			st = new StringTokenizer(br.readLine());
 			for(int w = 0; w < nWidth; w++)
 				buf[h][w] = Integer.parseInt(st.nextToken());
 		}
 		
+		for(int h = 1; h < nHeight+1; h++) 
+			for(int w = 1; w < nWidth+1; w++)
+				pSum[h][w] = pSum[h-1][w]+pSum[h][w-1]-pSum[h-1][w-1]+buf[h-1][w-1];
+		
 		int cnt = Integer.parseInt(br.readLine());
 		for(int i = 0; i < cnt; i++) {
-			int sum = 0;
 			st = new StringTokenizer(br.readLine());
-			int sttH = Integer.parseInt(st.nextToken())-1;
-			int sttW = Integer.parseInt(st.nextToken())-1;
+			int sttH = Integer.parseInt(st.nextToken());
+			int sttW = Integer.parseInt(st.nextToken());
 			int endH = Integer.parseInt(st.nextToken());
 			int endW = Integer.parseInt(st.nextToken());
-			for(int h = sttH; h < endH; h++) {
-				for(int w = sttW; w < endW; w++){
-					sum += buf[h][w];
-				}
-			}
-			sb.append(sum+"\n");
+
+			System.out.println(pSum[endH][endW] - pSum[sttH-1][endW] - pSum[endH][sttW-1] + pSum[sttH-1][sttW-1]);
 		}
-		System.out.println(sb);
+		
+		
+		//2차원 배열의 합
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		StringTokenizer st = new StringTokenizer(br.readLine());
+//		StringBuilder sb = new StringBuilder();
+//		int nHeight = Integer.parseInt(st.nextToken());
+//		int nWidth = Integer.parseInt(st.nextToken());
+//		int[][] buf = new int[nHeight][nWidth];
+//		for(int h = 0; h < nHeight; h++) {
+//			st = new StringTokenizer(br.readLine());
+//			for(int w = 0; w < nWidth; w++)
+//				buf[h][w] = Integer.parseInt(st.nextToken());
+//		}
+//		
+//		int cnt = Integer.parseInt(br.readLine());
+//		for(int i = 0; i < cnt; i++) {
+//			int sum = 0;
+//			st = new StringTokenizer(br.readLine());
+//			int sttH = Integer.parseInt(st.nextToken())-1;
+//			int sttW = Integer.parseInt(st.nextToken())-1;
+//			int endH = Integer.parseInt(st.nextToken());
+//			int endW = Integer.parseInt(st.nextToken());
+//			for(int h = sttH; h < endH; h++) {
+//				for(int w = sttW; w < endW; w++){
+//					sum += buf[h][w];
+//				}
+//			}
+//			sb.append(sum+"\n");
+//		}
+//		System.out.println(sb);
 		
 		
 		
