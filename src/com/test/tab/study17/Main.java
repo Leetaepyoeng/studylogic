6790a451471af6da07bc417261250a87cb52110d
Leetaepyoeng <48676338+Leetaepyoeng@users.noreply.github.com>
2024-02-18 10:01:02 +0900

    누적합

--- src/com/test/tab/study17/Main.java
+++ src/com/test/tab/study17/Main.java
@@ -7,29 +7,56 @@ import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		//구간 합 구하기 4
+		//구간 합 구하기 5
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		StringBuilder sb = new StringBuilder();
-		int nNum = Integer.parseInt(st.nextToken());
-		int nCnt = Integer.parseInt(st.nextToken());
-		int[] pSum = new int[nNum+1];
+		int wh = Integer.parseInt(st.nextToken());
+		int cnt = Integer.parseInt(st.nextToken());
+		int[][] arr = new int[wh][wh];
+		int[][] pSum = new int[wh+1][wh+1];
 		
-		st = new StringTokenizer(br.readLine());
-		for(int i = 1; i <= nNum; i++) {
-			pSum[0] = 0;
-			pSum[i] = pSum[i-1]+Integer.parseInt(st.nextToken());
-			//5 4 3 2 1 이면 처음에는 0+5 그다음은 [0+5]+4 그다음은 [0+5+4]+3순으로 감
-		}
-		
-		for(int i = 0; i < nCnt; i++) {
+		for(int i = 0; i < wh; i++) {
 			st = new StringTokenizer(br.readLine());
-			int stt = Integer.parseInt(st.nextToken());
-			int end = Integer.parseInt(st.nextToken());
-			sb.append(pSum[end]-pSum[stt-1]).append("\n");
+			for(int j = 0; j < wh; j++)
+				arr[i][j] = Integer.parseInt(st.nextToken());
 		}
+		for(int h = 1; h <= wh; h++)
+			for(int w = 1; w <= wh; w++)
+				pSum[h][w] = pSum[h-1][w]+pSum[h][w-1]-pSum[h-1][w-1]+arr[h-1][w-1];
 		
+		for(int i = 0; i < cnt; i++) {
+			st = new StringTokenizer(br.readLine());
+			int sttH = Integer.parseInt(st.nextToken());
+			int sttW = Integer.parseInt(st.nextToken());
+			int endH = Integer.parseInt(st.nextToken());
+			int endW = Integer.parseInt(st.nextToken());
+
+			sb.append(pSum[endH][endW] - pSum[sttH-1][endW] - pSum[endH][sttW-1] + pSum[sttH-1][sttW-1])
+			.append("\n");
+		}
 		System.out.println(sb);
+		
+		//구간 합 구하기 4
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		StringTokenizer st = new StringTokenizer(br.readLine());
+//		StringBuilder sb = new StringBuilder();
+//		int nNum = Integer.parseInt(st.nextToken());
+//		int nCnt = Integer.parseInt(st.nextToken());
+//		int[] pSum = new int[nNum+1];
+//		st = new StringTokenizer(br.readLine());
+//		for(int i = 1; i <= nNum; i++) {
+//			pSum[0] = 0;
+//			pSum[i] = pSum[i-1]+Integer.parseInt(st.nextToken());
+//			//5 4 3 2 1 이면 처음에는 0+5 그다음은 [0+5]+4 그다음은 [0+5+4]+3순으로 감
+//		}
+//		for(int i = 0; i < nCnt; i++) {
+//			st = new StringTokenizer(br.readLine());
+//			int stt = Integer.parseInt(st.nextToken());
+//			int end = Integer.parseInt(st.nextToken());
+//			sb.append(pSum[end]-pSum[stt-1]).append("\n");
+//		}
+//		System.out.println(sb);
 		
 		
 		//2차원 배열의 합 누적합 사용, 공식을 외우자
