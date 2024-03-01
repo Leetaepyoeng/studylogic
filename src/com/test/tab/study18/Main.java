270eda05098c79c446eedf1fd7fd0c7d7d2e4289
Leetaepyoeng <48676338+Leetaepyoeng@users.noreply.github.com>
2024-03-01 23:48:23 +0900

    기초

--- src/com/test/tab/study18/Main.java
+++ src/com/test/tab/study18/Main.java
@@ -3,41 +3,76 @@ package com.test.tab.study18;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		//숫자의 개수
+		//팩토리얼, ?공부
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		int num1 = Integer.parseInt(br.readLine());
-		int num2 = Integer.parseInt(br.readLine());
-		int num3 = Integer.parseInt(br.readLine());
-		int sum = num1 * num2 * num3;
-		int share = 0;
-		int rest = 0;
+		int cnt = Integer.parseInt(br.readLine());
+		int result = cnt;
 		
-		Map<Integer, Integer> map = new HashMap<>();
-		for(int i = 0; i < 10; i++)
-			map.put(i, 0);
+		if(cnt > 1)
+			for(int i = cnt-1; i > 0; i--) {
+				result *= i;
+			}
+		else
+			result = 1;
+		System.out.println(result);
 		
-		while(true) {
-			share = sum / 10;
-			rest = sum % 10;
-			
-			if(share == 0) {
-				map.put(rest, map.get(rest)+1);
-				break;
-			}
-			else {
-				map.put(rest, map.get(rest)+1);
-				sum /= 10;
-			}
-		}
-		for(int i = 0; i < 10; i++)
-			sb.append(map.get(i)).append("\n");
-		System.out.println(sb);
+		//평균은 넘겠지
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		StringBuilder sb = new StringBuilder();
+//		int cnt = Integer.parseInt(br.readLine());
+//		for(int i = 0; i < cnt; i++) {
+//			StringTokenizer st = new StringTokenizer(br.readLine());
+//			int scoreCnt = Integer.parseInt(st.nextToken());
+//			int sum = 0;
+//			int[] stCnt = new int[scoreCnt];
+//			for(int j = 0; j < scoreCnt; j++) {
+//				stCnt[j] = Integer.parseInt(st.nextToken());
+//				sum += stCnt[j];
+//			}
+//			float avg = (float)sum/scoreCnt;
+//			int overAvgCnt = 0;
+//			for(int j = 0; j < scoreCnt; j++)
+//				if(stCnt[j] > avg)
+//					overAvgCnt++;
+//			String result = String.format("%.3f%%\n",(float)overAvgCnt/scoreCnt*100);
+//			sb.append(result);
+//		}
+//		System.out.println(sb);
+		
+		
+		//숫자의 개수
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		StringBuilder sb = new StringBuilder();
+//		int num1 = Integer.parseInt(br.readLine());
+//		int num2 = Integer.parseInt(br.readLine());
+//		int num3 = Integer.parseInt(br.readLine());
+//		int sum = num1 * num2 * num3;
+//		int share = 0;
+//		int rest = 0;
+//		
+//		Map<Integer, Integer> map = new HashMap<>();
+//		for(int i = 0; i < 10; i++)
+//			map.put(i, 0);
+//		
+//		while(true) {
+//			share = sum / 10;
+//			rest = sum % 10;
+//			
+//			if(share == 0) {
+//				map.put(rest, map.get(rest)+1);
+//				break;
+//			}
+//			else {
+//				map.put(rest, map.get(rest)+1);
+//				sum /= 10;
+//			}
+//		}
+//		for(int i = 0; i < 10; i++)
+//			sb.append(map.get(i)).append("\n");
+//		System.out.println(sb);
 		
 		
 		//더하기 사이클
