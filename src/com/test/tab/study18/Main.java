f6b4a1c0383014b6dd9f278569e294cda37ced92
Leetaepyoeng <48676338+Leetaepyoeng@users.noreply.github.com>
2024-03-07 22:16:51 +0900

    기초

--- src/com/test/tab/study18/Main.java
+++ src/com/test/tab/study18/Main.java
@@ -3,7 +3,6 @@ package com.test.tab.study18;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException {
