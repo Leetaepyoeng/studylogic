5eee12461c7f0fa9f55ebb47423c59796e9cf515
Leetaepyoeng <48676338+Leetaepyoeng@users.noreply.github.com>
2024-03-21 14:20:02 +0900

    기초

--- src/com/test/tab/study18/Main.java
+++ src/com/test/tab/study18/Main.java
@@ -3,14 +3,26 @@ package com.test.tab.study18;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
 
 public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		//별 찍기 - 3
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
+		//
 		
+		
+		
+		
+		
+		
+		//별 찍기 - 3
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		StringBuilder sb = new StringBuilder();
+//		int cnt = Integer.parseInt(br.readLine());
+//		for(int i = 0; i < cnt; i++) {
+//			for(int j = cnt - i; j > 0 ; j--) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
 		
 		
 		//스택
