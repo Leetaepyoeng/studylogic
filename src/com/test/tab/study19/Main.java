c63b050c0e28988ee97b14e5abe75e4e7f1c450d
Leetaepyoeng <48676338+Leetaepyoeng@users.noreply.github.com>
2024-03-27 21:36:41 +0900

    시뮬레이션

---
+++ src/com/test/tab/study19/Main.java
@@ -0,0 +1,12 @@
+package com.test.tab.study19;
+
+//시뮬레이션
+public class Main {
+	public static void main(String[] args) {
+		//알고리즘 수업 - 알고리즘의 수행 시간 1, 알고리즘의 복잡도
+		System.out.println(1);
+        System.out.println(0);
+		
+		//프린터 큐 문제 이해를 못함.
+	}
+}
