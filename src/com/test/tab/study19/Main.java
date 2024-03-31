cff4bcb65d53c743c24a02aecefb409e199cb2c7
Leetaepyoeng <48676338+Leetaepyoeng@users.noreply.github.com>
2024-03-31 23:08:09 +0900

    시뮬레이션

--- src/com/test/tab/study19/Main.java
+++ src/com/test/tab/study19/Main.java
@@ -3,40 +3,90 @@ package com.test.tab.study19;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 //시뮬레이션
 public class Main {
 	public static void main(String[] args) throws IOException {
-		//만취한 상범
+		//트럭 주차, [contains():리스트에 특정 값이 있는지 확인]
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		int test = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+		int stt = 100;
+		int end = 0;
+		int price = 0;
+		List<Integer>[] list = new ArrayList[3];
 		
-		for(int tcnt = 0; tcnt < test; tcnt++)
-		{
-			int cnt = Integer.parseInt(br.readLine());
-			int jailbreak = 0;
-			boolean[] prison = new boolean[cnt+1];
-			for(int i = 1; i <= cnt; i++) {
-				if(i % 2 == 1)
-					prison[i] = true;
-			}
-			for(int i = 3; i <= cnt; i++) {
-				for(int j = i; j <= cnt; j++)
-					if(j % i == 0)
-						if(prison[j] == true)
-							prison[j] = false;
-						else
-							prison[j] = true;
-			}
-			
-			for(int i = 1; i <= cnt; i++) {
-				if(prison[i] == true)
-					jailbreak++;
-			}
-			sb.append(jailbreak).append("\n");
+		
+		for(int i = 0; i < 3; i++) {
+			list[i] = new ArrayList<>();
+			st = new StringTokenizer(br.readLine());
+			int sttNew = Integer.parseInt(st.nextToken());
+			int endNew = Integer.parseInt(st.nextToken());
+			if(stt > sttNew)
+				stt = sttNew;
+			if(end < endNew)
+				end = endNew;
+			for(int j = sttNew; j < endNew; j++)
+				list[i].add(j);
 		}
-		System.out.println(sb);
+		
+		for(int i = stt; i < end; i++) {
+			if(list[0].contains(i) && list[1].contains(i) && list[2].contains(i)) {
+				price += (c * 3);
+			}
+			else if((list[0].contains(i) && list[1].contains(i)) 
+					|| (list[0].contains(i) && list[2].contains(i)) 
+					|| (list[1].contains(i) && list[2].contains(i))) {
+				price += (b * 2);
+			}
+			else if(list[0].contains(i) || list[1].contains(i) || list[2].contains(i)){
+				price += a;
+			}
+			else {
+				;
+			}
+		}
+		System.out.println(price);
+		
+		
+		
+		
+		//만취한 상범
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		StringBuilder sb = new StringBuilder();
+//		int test = Integer.parseInt(br.readLine());
+//		for(int tcnt = 0; tcnt < test; tcnt++)
+//		{
+//			int cnt = Integer.parseInt(br.readLine());
+//			int jailbreak = 0;
+//			boolean[] prison = new boolean[cnt+1];
+//			for(int i = 1; i <= cnt; i++) {
+//				if(i % 2 == 1)
+//					prison[i] = true;
+//			}
+//			for(int i = 3; i <= cnt; i++) {
+//				for(int j = i; j <= cnt; j++)
+//					if(j % i == 0)
+//						if(prison[j] == true)
+//							prison[j] = false;
+//						else
+//							prison[j] = true;
+//			}
+//			
+//			for(int i = 1; i <= cnt; i++) {
+//				if(prison[i] == true)
+//					jailbreak++;
+//			}
+//			sb.append(jailbreak).append("\n");
+//		}
+//		System.out.println(sb);
+		
+		
 		
 		
 		//카드 역배치, [리스트, 컬렉션 리버스]
